1c0441d1ba92f5039581580f49976d707c30939c
MaryamZ <mbiheiri@gmail.com>
2020-08-28 17:23:06 -0500

    v2

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 //Selectors
+<script language="JavaScript">
+
+var frmvalidator = new Validator(“contactform”); 
+frmvalidator.addValidation(“name”,”req”,”Please provide your name”); 
+frmvalidator.addValidation(“email”,”req”,”Please provide your email”); 
+frmvalidator.addValidation(“email”,”email”, “Please enter a valid email address”); 
+</script>
