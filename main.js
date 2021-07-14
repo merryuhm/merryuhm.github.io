e99e42c08cc619248907e944ee409e1200014335
MaryamZ <mbiheiri@gmail.com>
2021-07-14 10:47:15 -0500

    v3
    2021 job rounds

--- main.js
+++
@@ -1,6 +0,0 @@
-let header = document.querySelector('.header');
-
-window.addEventListener('scroll', function () {
-    let windowPosition = window.scrollY > 0;
-    header.classList.toggle('active', windowPosition)
-})
